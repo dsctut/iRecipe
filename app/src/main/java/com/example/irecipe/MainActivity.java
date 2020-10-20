a3909412252bb7b4f14534122d5e875736392d47
[PrinceBLVCK] <princekarabo70@gmail.com>
2020-10-21 00:51:52 +0200

    initial Commit

--- app/src/main/java/com/example/irecipe/MainActivity.java
+++ app/src/main/java/com/example/irecipe/MainActivity.java
@@ -10,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, homeActivity.class));
+                finish();
+            }
+        };
+
+        new Handler().postDelayed(run,2000);
+
     }
 }
